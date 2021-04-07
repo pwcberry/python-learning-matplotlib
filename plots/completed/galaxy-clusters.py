b6ad3996c925a323cc9499221f4d031d1737c93d
Peter Berry <peter.berry@rea-group.com>
2021-04-07 23:49:48 +1000

    Tidy up plots and add a combination plot

--- plots/completed/galaxy-clusters.py
+++ plots/completed/galaxy-clusters.py
@@ -1,8 +1,20 @@
 # import pandas as pd
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from astropy import units as u
-from astropy.coordinates import SkyCoord, Angle
+from astropy.coordinates import SkyCoord, Angle, Galactocentric
 from astropy.io import ascii
+from astropy.cosmology import WMAP9 as cosmo
+
+
+def to_galacto(long, lat, redshift):
+    distance = cosmo.comoving_distance(redshift)
+    sc = SkyCoord(l=long * u.degree, b=lat * u.degree,
+                  distance=distance, frame='galactic')
+    gc = sc.transform_to(Galactocentric)
+    return [gc.x.value, gc.y.value, gc.z.value]
+
 
 # df = pd.read_csv('abell-galaxy-clusters-simplified.csv')
 tbl = ascii.read('abell-galaxy-clusters-simplified.csv')
@@ -12,10 +24,11 @@ long = Angle(tbl['GLON_2000'] * u.degree)
 long = long.wrap_at(180 * u.degree)
 lat = Angle(tbl['GLAT_2000'] * u.degree)
 
-fig = plt.figure(figsize=(16,12))
-ax = fig.add_subplot(111, projection = 'mollweide')
-ax.scatter(long.radian, lat.radian)
-ax.set_xticklabels(['210','240','270','300','330','0','30','60','90','120','150'])
+fig = plt.figure(figsize=(12, 9))
+ax = fig.add_subplot(111, projection='mollweide')
+ax.scatter(long.radian, lat.radian, s=3, c='#b00020')
+ax.set_xticklabels(['210°', '240°', '270°', '300°', '330°',
+                    '0°', '30°', '60°', '90°', '120°', '150°'])
 ax.grid(True)
 
 plt.show()
@@ -24,3 +37,20 @@ plt.show()
 # Will need math to transform coordinates from 3D to 2D
 # Also perhaps transform coordinates?
 
+data = pd.read_csv('../../data/abell-galaxy-clusters-simplified.csv')
+rds = data[data['Redshift'].notna()]
+
+cs = np.array([
+    to_galacto(row.GLON_2000, row.GLAT_2000, row.Redshift)
+    for row in rds.itertuples()
+])
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(cs[:, 0], cs[:, 1], cs[:, 2], s=3, c='#b00020')
+ax.set_xlabel('Mpc')
+ax.set_ylabel('Mpc')
+ax.set_zlabel('Mpc')
+ax.grid(True)
+
+plt.show()
--- plots/completed/star_cluster_plot.py
+++ plots/completed/star_cluster_plot.py
@@ -3,46 +3,61 @@ import pandas as pd
 import astropy.coordinates as coord
 import astropy.units as u
 import matplotlib.pyplot as plt
+from math import pi
 
-data = pd.read_csv('./star-clusters.csv', index_col = 0)
-filter = data['d'] < 2000
+
+def to_radians(deg):
+    return deg / 180 * pi
+
+
+data = pd.read_csv('../../data/star-clusters.csv', index_col=0)
+filter = data['d'] < 20000
 subset = data[filter]
 
 cs = np.array(
     [
-        coord.SkyCoord(ra=row.RAhour * u.hourangle, dec=row.DEdeg * u.degree, distance = row.d * u.pc)
+        coord.SkyCoord(ra=row.RAhour * u.hourangle,
+                       dec=row.DEdeg * u.degree, distance=row.d * u.pc)
         for row in subset.itertuples()
     ]
 )
 
 # Plot clusters to galacto-centric coordinates
 # These are cartesian x,y,z coordinates
-gcs = np.array([ c.transform_to(coord.Galactocentric) for c in cs ])
+gcs = np.array([c.transform_to(coord.Galactocentric) for c in cs])
 
 # Transform them into spherical coordinates
 sphs = np.array([
-    [g.spherical.lon.value, g.spherical.lat.value, g.spherical.distance.value]
+    [
+        to_radians(g.spherical.lon.value),
+        to_radians(g.spherical.lat.value),
+        g.spherical.distance.value
+    ]
     for g in gcs
 ])
 
-fig = plt.figure(figsize=(18,9))
-ax1 = fig.add_subplot(1,2,1, projection='polar')
+fig = plt.figure(figsize=(18, 9))
+ax1 = fig.add_subplot(1, 2, 1, projection='polar')
 ax1.set_title('Galactocentric')
 ax1.set_xlabel('longitude')
-ax1.set_yticklabels(['','4Kpc','','8Kpc','',''])
-ax1.scatter(sphs[:,0], sphs[:,2], s=3, c='#FFCC33')
+ax1.set_yticklabels(['', '5kpc', '', '10kpc', '', '15kpc', '', '20kpc'])
+ax1.scatter(sphs[:, 0], sphs[:, 2], s=3, c='#FFCC33')
 
 # Plot galactic longitude with distance from our solar system
 gxs = np.array([
-    [c.galactic.l.value, c.galactic.b.value, c.distance.value]
+    [
+        to_radians(c.galactic.l.value),
+        to_radians(c.galactic.b.value),
+        c.distance.value
+    ]
     for c in cs
 ])
 
 ax2 = fig.add_subplot(122, projection='polar')
 ax2.set_title('Galactic coordinates')
 ax2.set_xlabel('longitude')
-ax2.set_yticklabels(['','','','1Kpc','','','','2Kpc','',''])
-ax2.scatter(gxs[:,0], gxs[:,2], s=3, c='#109910')
+ax2.set_yticklabels(['', '5kpc', '', '10kpc', '', '15kpc', '', '20kpc'])
+ax2.scatter(gxs[:, 0], gxs[:, 2], s=3, c='#109910')
 
 plt.show()
 
